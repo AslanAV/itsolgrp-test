42e3f38494ddab954c19553623dd571899c359aa
AslanAV <autlevaslan@yandex.ru>
2023-05-20 19:05:52 +0300

    rename view welcome to main_page

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -9,6 +9,6 @@ class MainPageController extends Controller
     public function index()
     {
         $articles = Article::orderBy('id', 'desc')->limit(6)->get();
-        return view('welcome', compact('articles'));
+        return view('main_page', compact('articles'));
     }
 }
