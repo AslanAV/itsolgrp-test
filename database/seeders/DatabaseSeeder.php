a44dcd41a21e2774ec9e1d142e47db2c9f33857f
AslanAV <autlevaslan@yandex.ru>
2023-05-22 01:01:41 +0300

    any fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ArticleSeeder::class,
 //            CommentSeeder::class,
-            TagSeeder::class,
+//            TagSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
