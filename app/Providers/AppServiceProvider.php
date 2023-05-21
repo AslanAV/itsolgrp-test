9d9f100bf1d0cbdc99d02d20de9a8e97499011e3
AslanAV <autlevaslan@yandex.ru>
2023-05-22 01:11:58 +0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') !== 'local') {// Default value should be false for local server
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrapFive();
     }
 }
