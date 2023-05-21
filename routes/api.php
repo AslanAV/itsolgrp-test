78d2fb60543eb434f750d0e66199e6eeac271447
AslanAV <autlevaslan@yandex.ru>
2023-05-21 19:06:11 +0300

    add api route for likes and views

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\LikeController;
+use App\Http\Controllers\API\ViewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/likes', [LikeController::class, 'store'])->name('api_likes_get');
+
 Route::post('/likes', [LikeController::class, 'store'])->name('api_likes_post');
+Route::post('/views', [ViewController::class, 'store'])->name('api_views_post');
